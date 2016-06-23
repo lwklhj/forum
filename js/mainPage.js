bafab086ffa2b9af32b14bb802f983e37071e850
hhfff <hefei.he95@gmail.com>
2016-06-23 15:01:59 +0800

    add pic to each course and cca
    add pic to each course and cca

--- js/mainPage.js
+++ js/mainPage.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	$(document).on("click",".menu_wrapper a",function(){
 		var 
 		$menuList=$(this).parents(".menu_wrapper").children("a");
-		$itemList=$(this).parents(".menu_container").siblings(".item_container").children("ul");
+		$itemList=$(this).parents(".menu_container").siblings(".item_container").children("table");
 		index=$menuList.index(this);
-		$itemList.eq(index).show().siblings("ul").hide();
+		$itemList.eq(index).show(1000).siblings("table").hide();
 
 	});
-	$(".item_container ul:first-child").show();
+	$(".item_container table:first-child").show();
 
 	
 	

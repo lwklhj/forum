6e974b56d06056ee9cdbca425c1e0d03be7c5426
hhfff <hefei.he95@gmail.com>
2016-07-01 14:21:12 +0800

    ha

--- js/forumThread.js
+++ js/forumThread.js
@@ -7,8 +7,8 @@ var getQueryString=function(field,url){
 
 var queryData=[getQueryString("type"),getQueryString("name")];
 $(document).ready(function(){
-	for(var i=0;i<20;i++){
-		var newRow="<tr><td class=\"name\"><a href=\"threadpage.html\">haha</a></td><td class=\"by\">bb</td><td class=\"date\">date</td></tr>";
+	for(var i=1;i<=20;i++){
+		var newRow="<tr><td class=\"name\"><a href=\"threadpage.html\">Thread "+i+"</a></td><td class=\"by\">bb</td><td class=\"date\">date</td></tr>";
 		$("#content").append(newRow);
 	}
 

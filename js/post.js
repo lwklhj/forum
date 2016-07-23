803c43c491e85bd6268d9e90dc46585261b81627
hhfff <hefei.he95@gmail.com>
2016-07-23 21:23:04 +0800

    ha

--- js/post.js
+++ js/post.js
@@ -2,6 +2,7 @@ var getQueryString=function(field,url){
     var href=url ? url:window.location.href;
     var reg=new RegExp("[?&]"+field+ "=([^&#]*)","i");
     var string=reg.exec(href);
+    console.log(string[1]);
     return string ? string[1]:null;
 }
 

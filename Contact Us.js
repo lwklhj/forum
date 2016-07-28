fea2c123bc27fa47c0381cfbd6f40a58e8a402fa
lwklhj <woonkit.liu.sg@gmail.com>
2016-07-28 23:04:45 +0800

    Add files via upload

---
+++ Contact Us.js
@@ -0,0 +1,27 @@
+function switcher_1() {
+	if(document.getElementById("switch_1").style.display == "none")
+		document.getElementById("switch_1").style.display = "block";
+	else
+		document.getElementById("switch_1").style.display = "none";
+}
+
+function switcher_2() {
+	if(document.getElementById("switch_2").style.display == "none")
+		document.getElementById("switch_2").style.display = "block";
+	else
+		document.getElementById("switch_2").style.display = "none"
+}
+
+function switcher_3() {
+	if(document.getElementById("switch_3").style.display == "none")
+		document.getElementById("switch_3").style.display = "block";
+	else
+		document.getElementById("switch_3").style.display = "none"
+}
+
+function switcher_4() {
+	if(document.getElementById("switch_4").style.display == "none")
+		document.getElementById("switch_4").style.display = "block";
+	else
+		document.getElementById("switch_4").style.display = "none"
+}

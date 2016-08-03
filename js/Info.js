8d2829a17be7dfa8aa9d81d27a2905cf0f3c6d59
lwklhj <woonkit.liu.sg@gmail.com>
2016-08-03 20:43:23 +0800

    Update Info.js

--- js/Info.js
+++ js/Info.js
@@ -12,22 +12,7 @@ function switcher_2() {
 		document.getElementById("switch_2").style.display = "none"
 }
 
-function switcher_3() {
-	if(document.getElementById("switch_3").style.display == "none")
-		document.getElementById("switch_3").style.display = "block";
-	else
-		document.getElementById("switch_3").style.display = "none"
-}
-
-function switcher_4() {
-	if(document.getElementById("switch_4").style.display == "none")
-		document.getElementById("switch_4").style.display = "block";
-	else
-		document.getElementById("switch_4").style.display = "none"
-}
-
 function NYP_tutorial_on() {
-	localStorage.setItem("", 1);
+	localStorage.removeItem("j");
 	document.location.href = "mainpage.html";
-	
 }
